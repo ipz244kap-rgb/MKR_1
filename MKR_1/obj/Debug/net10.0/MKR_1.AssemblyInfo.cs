786e1216916dd38268d6aa640a21f39d5232638e
ALina Kulykivska <ipz244_kap@student.ztu.edu.ua>
2026-05-22 00:57:52 +0300

    Iterator

--- MKR_1/obj/Debug/net10.0/MKR_1.AssemblyInfo.cs
+++ MKR_1/obj/Debug/net10.0/MKR_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MKR_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706899d35b20b008a5beb581a075fd3166f6858d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MKR_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MKR_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
