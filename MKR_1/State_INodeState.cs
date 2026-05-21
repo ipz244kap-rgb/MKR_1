64c45a7b3820cfe51c0885fa1e6f21ebda0d3109
ALina Kulykivska <ipz244_kap@student.ztu.edu.ua>
2026-05-22 01:18:04 +0300

    state

---
+++ MKR_1/State_INodeState.cs
@@ -0,0 +1,7 @@
+namespace StructuralPatternsLab
+{
+    public interface State_INodeState
+    {
+        string Render(Composite_LightElementNode node);
+    }
+}
--- MKR_1/obj/Debug/net10.0/MKR_1.AssemblyInfo.cs
+++ MKR_1/obj/Debug/net10.0/MKR_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MKR_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786e1216916dd38268d6aa640a21f39d5232638e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5bd90afb8ce9e0636e8846ddd63b2735cd1e0bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MKR_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MKR_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
